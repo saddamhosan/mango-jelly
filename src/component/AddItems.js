ec2a5ed1a5e35746b64cf54daa2c3f9da705cb46
saddamhosan <contact.saddamhosan.com>
2022-06-26 22:15:34 +0600

    added add items

--- src/component/AddItems.js
+++ src/component/AddItems.js
@@ -1,9 +1,63 @@
 
 const AddItems = () => {
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        const name =e.target.name.value
+        const description = e.target.description.value;
+        const ram = e.target.ram.value;
+        const internalStorage = e.target.internalStorage.value;
+        const screenSize = e.target.screenSize.value;
+        const mobile = {
+          name,
+          description,
+          ram,
+          internalStorage,
+          screenSize,
+        };
+        console.log(mobile);
+    }
     return (
-        <div>
-            <h1>AddItems</h1>
-        </div>
+        
+      <div>
+        <form onSubmit={handleSubmit} className="text-center">
+        <h1>AddItems</h1>
+          <input
+            type="text"
+            name="name"
+            placeholder="Brand Name"
+            className="border-2 rounded-xl w-1/3 p-2 m-2"
+          />
+          <br />
+          <textarea
+            name="description"
+            placeholder="Description"
+            className="border-2 rounded-xl w-1/3 p-2 m-2"
+          />
+          <br />
+          <input
+            type="text"
+            name="ram"
+            placeholder="RAM"
+            className="border-2 rounded-xl w-1/3 p-2 m-2"
+          />
+          <br />
+          <input
+            type="text"
+            name="internalStorage"
+            placeholder="Internal Storage"
+            className="border-2 rounded-xl w-1/3 p-2 m-2"
+          />
+          <br />
+          <input
+            type="text"
+            name="screenSize"
+            placeholder="Screen Size"
+            className="border-2 rounded-xl w-1/3 p-2 m-2"
+          />
+          <br />
+          <input type="submit" value="Add Item" className="btn btn-md" />
+        </form>
+      </div>
     );
 };
 
