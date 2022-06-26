dc93f3d77aca05a985d16b2a18c33357b1f4147e
saddamhosan <contact.saddamhosan.com>
2022-06-26 21:25:07 +0600

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import AddItems from "./component/AddItems";
+import Home from "./component/Home";
 
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/addItems" element={<AddItems/>}/>
+      </Routes>
     </div>
   );
 }
